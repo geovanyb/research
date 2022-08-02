f59a77f80c1a91d21651b03f4661dd47a39840e5
Lucas2310-hub <61985406+Lucas2310-hub@users.noreply.github.com>
2022-08-02 15:52:28 -0300

    SFM atualization v1
    Structure of code is fineshed
    Next phase: functions development or complete

--- quant-finance/splits-and-merge/Split-and-merge.py
+++ quant-finance/splits-and-merge/Split-and-merge.py
@@ -14,26 +14,25 @@ def PbFuzzyClustering(data, Nc):
     betas = np.ones(Nc,2) # Vetor de protótipos
     weights = np.ones(Nc, Nd) # Vetor de pesos Protótipos x Pontos
     dist  = np.ones(Nc, Nd) # Vetor de distâncias Protótipos x Pontos
-    
-    for C in range(Nc):
-        tx  = np.average(data[cD[0]],weights=wF)
-        ty  = np.average(data[cD[1]],weights=wF)
-        Sxx = (un**m*(data[cD[0]]-tx)**2).sum()
-        Syy = (un**m*(data[cD[1]]-ty)**2).sum()
-        Sxy = (un**m*(data[cD[0]]-tx)*(data[cD[1]]-ty)).sum()
+    for i in range(Nd):
+        dist[:i] = betas[:0]-data[:0]*np.cos(betas[:1])-data[:1]*np.sin(betas[:1])
         
     while lim:
-        for i in range(Nd):
-            dist[:i] = betas[:0]-data[:0]*np.cos(betas[:1])-data[:1]*np.sin(betas[:1])
-        __weightFuzzy(data,betai,g)
-        __nbeta(data,tx,ty,Sxx,Syy,Sxy)
+        # Cálculo dos novos pesos a partir das distâncias e estatísticas anteriores
+        Wf = __weightFuzzy(data,betai,g)
+        # Cálculo das estatísticas com os novos pesos
         tx  = np.average(data[0],weights=wF)
         ty  = np.average(data[1],weights=wF)
         Sxx = (un**m*(data[0]-tx)**2).sum()
         Syy = (un**m*(data[1]-ty)**2).sum()
         Sxy = (un**m*(data[0]-tx)*(data[1]-ty)).sum()
-
-def __weightFuzzy(data, tx, ty, dist): # Função que atribui os pesos uij (ponto j para a protótipo i)
+        # Cálculo dos novos betas a partir dos pesos e estatísticas
+        betas = __nbeta(data,tx,ty,Sxx,Syy,Sxy)
+        # Cálculo das distâncias dos dados para os novos betas
+        for i in range(Nd):
+            dist[:i] = betas[:0]-data[:0]*np.cos(betas[:1])-data[:1]*np.sin(betas[:1])
+        
+def __weightFuzzy(data, tx, ty, dist, betai, betas, m): # Função que atribui os pesos uij (ponto j para a protótipo i)
         
     g2    = (data[:0]-tx)**2+(data[0:1]-ty)**2
     dad2i = dist**2+g2
@@ -50,3 +49,8 @@ def __nbeta(data, tx, ty, Sxx, Syy, Sxy):
     rhoi = tx*np.cos(alpi)+ty*np.sin(alpi)
     
     return (rhoi,alpi)
+
+def __distance(data,betai):
+    d = betai[0]-data[0]*np.cos(betai[1])-data[1]*np.sin(betai[1])
+    return d
+    
