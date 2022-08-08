fea589925a0a846ede439bb80d18e7ae00a000f6
Lucas2310-hub <61985406+Lucas2310-hub@users.noreply.github.com>
2022-08-08 11:18:59 -0300

    Update While
    Update the while structure. Next step is to test the code before continue the changes, with finishing __weightFuzzy function in sequence.

--- quant-finance/splits-and-merge/Split-and-merge.py
+++ quant-finance/splits-and-merge/Split-and-merge.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 
-def PbFuzzyClustering(data, Nc, m):
+def PbFuzzyClustering(data, Nc, m, iter_limit):
 # Modelo de clusterização por linha
 # O que deve ser feito iterativamente:
         # inicialmento setar os protótipos iniciais
@@ -22,30 +22,30 @@ def PbFuzzyClustering(data, Nc, m):
     
     for i in range(Nd):
         dist[:i] = betas[:0]-data[:0]*np.cos(betas[:1])-data[:1]*np.sin(betas[:1])
-        
+    
+    iL = 0
+    lim = 1
     while lim:
         # Cálculo dos novos pesos a partir das distâncias e estatísticas anteriores
         Wf = __weightFuzzy(data,betai,g)
         # Cálculo das estatísticas com os novos pesos
-        tx  = np.average(data[0],weights=wF)
-        ty  = np.average(data[1],weights=wF)
-        Sxx = (un**m*(data[0]-tx)**2).sum()
-        Syy = (un**m*(data[1]-ty)**2).sum()
-        Sxy = (un**m*(data[0]-tx)*(data[1]-ty)).sum()
+        tx, ty, Sxx, Syy, Sxy = __estatistics(data,Wf,m)
         # Cálculo dos novos betas a partir dos pesos e estatísticas
         betas = __nbeta(data,tx,ty,Sxx,Syy,Sxy)
         # Cálculo das distâncias dos dados para os novos betas
         for i in range(Nd):
             dist[:i] = betas[:0]-data[:0]*np.cos(betas[:1])-data[:1]*np.sin(betas[:1])
+        iL += 1
+        if iL = iter_limit:
+            lim = 0
         
 def __weightFuzzy(data, tx, ty, dist, betai, betas, m): # Função que atribui os pesos uij (ponto j para a protótipo i)
         
-    g2    = (data[:0]-tx)**2+(data[0:1]-ty)**2
+    g2    = (data[:0]-tx)**2+(data[:1]-ty)**2
     dad2i = dist**2+g2
     for bet in betas: 
         fj  = bet[0] - row[0]*np.cos(bet[1])-dataj[1]*np.sin(bet[1])
         d2j = fj**2+g**2 
-        
         
     return d
 
