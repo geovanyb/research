6e801930d4c1c9ac87d9bc43a63555789391a069
Lucas2310-hub <61985406+Lucas2310-hub@users.noreply.github.com>
2022-08-15 14:34:59 -0300

    Estatistics function correction
    Correction of __estatistics function.

--- quant-finance/splits-and-merge/Split-and-merge.py
+++ quant-finance/splits-and-merge/Split-and-merge.py
@@ -9,8 +9,8 @@ def PbFuzzyClustering(data, Nc, m, iter_limit):
         # calcular os pesos do cluster
         # reajustar os protótipos
     
-    cD      = data.columns
-    Nd      = data.size[0]
+    Nc      = data.shape[1]
+    Nd      = data.shape[0]
     Nb0     = int(Nd/Nc)
     dtb0    = [(Nb0*i,Nb0*i+1) for i in range(Nc)]
     weights = np.ones(Nc, Nd) # Vetor de pesos Protótipos x Pontos
@@ -41,7 +41,7 @@ def PbFuzzyClustering(data, Nc, m, iter_limit):
         
 def __weightFuzzy(data, tx, ty, dist, betai, betas, m): # Função que atribui os pesos uij (ponto j para a protótipo i)
         
-    g2    = (data[:0]-tx)**2+(data[:1]-ty)**2
+    g2    = (data[:,0]-tx)**2+(data[:,1]-ty)**2
     dad2i = dist**2+g2
     for bet in betas: 
         fj  = bet[0] - row[0]*np.cos(bet[1])-dataj[1]*np.sin(bet[1])
@@ -60,11 +60,20 @@ def __distance(data,betai):
     d = betai[0]-data[0]*np.cos(betai[1])-data[1]*np.sin(betai[1])
     return d
 
+## Função que entrega as estatísticas dos dados enviados (dados 2D)
 def __estatistics(data, wF, m):
-    tx  = np.average(data[0],weights=wF**m)
-    ty  = np.average(data[1],weights=wF**m)
-    Sxx = (wF**m*(data[0]-tx)**2).sum()
-    Syy = (wF**m*(data[1]-ty)**2).sum()
-    Sxy = (wF**m*(data[0]-tx)*(data[1]-ty)).sum()
-    return tx, ty, Sxx, Syy, Sxy        
+    tx  = np.empty((wF.shape[0],0))
+    ty  = np.empty((wF.shape[0],0))
+    Sxx = np.empty((wF.shape[0],0))
+    Syy = np.empty((wF.shape[0],0))
+    Sxy = np.empty((wF.shape[0],0))
+    
+    for bt in range(wF.shape[0]):
+        tx  = np.append(tx,np.average(data[:,0],weights=wF[bt,:]**m))
+        ty  = np.append(ty,np.average(data[:,1],weights=wF[bt,:]**m))
+        Sxx = np.append(Sxx, (wF[bt,:]**m*(data[:,0]-tx[bt])**2).sum())
+        Syy = np.append(Syy, (wF[bt,:]**m*(data[:,1]-ty[bt])**2).sum())
+        Sxy = np.append(Sxy, (wF[bt,:]**m*(data[:,0]-tx[bt])*(data[:,1]-ty[bt])).sum())
+        
+    return tx, ty, Sxx, Syy, Sxy       
     
